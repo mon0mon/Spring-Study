561dcdacfa66797b44c1bbead79a951a9c68c1bd
mon0mon <mon0mon@naver.com>
2025-02-08 16:21:54 +0900

    #3 Spring RestDocs - OpenAPI - Swagger init : 학습 프로젝트 추가

---
+++ Documentation/RestDocs-OpenAPI-Swagger/self-restdocs-swagger/src/main/kotlin/xyz/mon0mon/selfrestdocsswagger/SelfRestdocsSwaggerApplication.kt
@@ -0,0 +1,11 @@
+package xyz.mon0mon.selfrestdocsswagger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelfRestdocsSwaggerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SelfRestdocsSwaggerApplication>(*args)
+}
