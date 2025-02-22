a9f0430e24c4f3589cae623802fffd65c9de44cd
mon0mon <mon0mon@naver.com>
2025-02-22 20:01:24 +0900

    #7 Spring Websocket - STOMP Communication init: WebSockets With Spring part3 - Java Spring 예제 프로젝트 생성

---
+++ Websocket/medium/SpringWebSocketSTOMPClient/src/main/java/xyz/mon0mon/springwebsocketstompclient/SpringWebSocketStompClientApplication.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.springwebsocketstompclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketStompClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSocketStompClientApplication.class, args);
+    }
+
+}
---
+++ Websocket/medium/SpringWebSocketSTOMPClient/src/test/java/xyz/mon0mon/springwebsocketstompclient/SpringWebSocketStompClientApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.springwebsocketstompclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketStompClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
