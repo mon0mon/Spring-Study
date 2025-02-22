f7e683672cd1ebd95de57e33f8aceea8e37b5b55
mon0mon <mon0mon@naver.com>
2025-02-22 12:18:13 +0900

    #7 Spring Websocket - STOMP Communication init: WebSockets With Spring part2 - 신규 프로젝트 생성

---
+++ Websocket/medium/SpringWebSocketSockJS/src/main/java/xyz/mon0mon/springwebsocketsockjs/SpringWebSocketSockJsApplication.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.springwebsocketsockjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketSockJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSocketSockJsApplication.class, args);
+    }
+
+}
