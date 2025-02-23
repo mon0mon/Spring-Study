e7d1a924113680ae6628deaaf6f3f0310b4126fd
mon0mon <mon0mon@naver.com>
2025-02-23 22:31:22 +0900

    #7 Spring Websocket - STOMP Communication feat: Build a Real-Time Chat App From Scratch - chatmessage 리포지토리, 서비스 생성

---
+++ Websocket/youtube/bouali_ali/websocket/src/main/java/xyz/mon0mon/websocket/chat/ChatMessageService.java
@@ -0,0 +1,33 @@
+package xyz.mon0mon.websocket.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import xyz.mon0mon.websocket.chatroom.ChatRoomService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final ChatMessageRepository repository;
+    private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        var chatId = chatRoomService.getChatRoomId(
+                chatMessage.getSenderId(), chatMessage.getRecipientId(), true
+        ).orElseThrow();    // 커스텀 예외를 발생 가능
+
+        chatMessage.setChatId(chatId);
+        repository.save(chatMessage);
+
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
+}
