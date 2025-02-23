46d043a5f1cb10359fe7e9a287045c5eb0166a5a
mon0mon <mon0mon@naver.com>
2025-02-23 22:13:10 +0900

    #7 Spring Websocket - STOMP Communication feat: Build a Real-Time Chat App From Scratch - user 리포지토리, 서비스 생성

---
+++ Websocket/youtube/bouali_ali/websocket/src/main/java/xyz/mon0mon/websocket/user/UserRepository.java
@@ -0,0 +1,9 @@
+package xyz.mon0mon.websocket.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findAllByStatus(Status status);
+}
