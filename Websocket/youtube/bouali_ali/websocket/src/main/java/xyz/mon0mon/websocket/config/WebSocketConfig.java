fbca22f2b713949fdc4c17bda162f18df785f156
mon0mon <mon0mon@naver.com>
2025-02-23 22:12:34 +0900

    #7 Spring Websocket - STOMP Communication feat: Build a Real-Time Chat App From Scratch - WebSocket 설정 추가

---
+++ Websocket/youtube/bouali_ali/websocket/src/main/java/xyz/mon0mon/websocket/config/WebSocketConfig.java
@@ -0,0 +1,52 @@
+package xyz.mon0mon.websocket.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // STOMP endpoint 설정
+        registry.addEndpoint("/ws");
+        //                .withSockJS();
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(APPLICATION_JSON);
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+
+        messageConverters.add(converter);
+
+        // 기본 제공되는 컨버터들을 사용하지 않음
+        return false;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메시지 브로커 url 설정
+        registry.enableSimpleBroker("/user");
+        // 메시지를 받을 api의 prefix 설정
+        registry.setApplicationDestinationPrefixes("/app");
+        // 유저별로 메시지를 보낼 때 사용할 prefix 설정
+        registry.setUserDestinationPrefix("/user");
+    }
+}
