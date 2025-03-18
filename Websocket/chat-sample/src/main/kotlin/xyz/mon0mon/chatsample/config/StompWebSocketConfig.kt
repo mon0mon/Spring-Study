23ba29187cbd17450c121ebf5256b703b5ca4b4e
mon0mon <mon0mon@naver.com>
2025-03-19 02:57:26 +0900

    #7 Spring Websocket - STOMP Communication project: Sample Chat 프로젝트 - STOMP 웹 소켓 설정 추가

---
+++ Websocket/chat-sample/src/main/kotlin/xyz/mon0mon/chatsample/config/StompWebSocketConfig.kt
@@ -0,0 +1,22 @@
+package xyz.mon0mon.chatsample.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class StompWebSocketConfig: WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws/stomp")
+            .setAllowedOrigins("*")
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/topic")
+        registry.setApplicationDestinationPrefixes("/app")
+    }
+}
