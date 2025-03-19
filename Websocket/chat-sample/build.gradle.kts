bd5a820d6921df18e1d34944b32384cc2320a086
mon0mon <mon0mon@naver.com>
2025-03-19 12:15:42 +0900

    #7 Spring Websocket - STOMP Communication project: Sample Chat 프로젝트 - Spring-Security-Messaging 의존성 추가

--- Websocket/chat-sample/build.gradle.kts
+++ Websocket/chat-sample/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-messaging")
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
