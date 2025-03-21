825259004cf1b9ada54bcd13a661bf31c0377ab0
Max Lee <max.lee@neoventures.kr>
2025-03-21 11:36:59 +0900

    #7 Spring Websocket - STOMP Communication feat: Sample Chat 프로젝트 - periodic 메시지 주기 변경 - 초기 데이터 변경

--- Websocket/chat-sample/src/main/kotlin/xyz/mon0mon/chatsample/ChatSampleApplication.kt
+++ Websocket/chat-sample/src/main/kotlin/xyz/mon0mon/chatsample/ChatSampleApplication.kt
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.crypto.password.PasswordEncoder
 import xyz.mon0mon.chatsample.domain.chat.ChatRoom
+import xyz.mon0mon.chatsample.domain.chat.ChatRoomParticipant
 import xyz.mon0mon.chatsample.domain.user.User
+import xyz.mon0mon.chatsample.repository.chat.ChatRoomParticipantRepository
 import xyz.mon0mon.chatsample.repository.chat.ChatRoomRepository
 import xyz.mon0mon.chatsample.repository.user.UserRepository
 
@@ -19,20 +21,25 @@ private val logger = KotlinLogging.logger { }
 class ChatSampleApplication {
 
     @Bean
-    fun init(userRepository: UserRepository, chatRoomRepository: ChatRoomRepository, passwordEncoder: PasswordEncoder) =
+    fun init(
+        userRepository: UserRepository, chatRoomRepository: ChatRoomRepository,
+        chatRoomParticipantRepository: ChatRoomParticipantRepository, passwordEncoder: PasswordEncoder
+    ) =
         CommandLineRunner {
-            val user1 = User(name = "user1", email = "user1@example.com", password = passwordEncoder.encode("1234"))
-            val user2 = User(name = "user2", email = "user2@example.com", password = passwordEncoder.encode("1234"))
-
-            val chatRoom1 = ChatRoom(name = "chatroom-1", owner = user1)
-            val chatRoom2 = ChatRoom(name = "chatroom-2", owner = user2)
-
             try {
-                userRepository.save(user1)
-                userRepository.save(user2)
+                val user1 = userRepository.save(
+                    User(name = "user1", email = "user1@example.com", password = passwordEncoder.encode("1234")))
+                val user2 = userRepository.save(
+                    User(name = "user2", email = "user2@example.com", password = passwordEncoder.encode("1234")))
 
-                chatRoomRepository.save(chatRoom1)
-                chatRoomRepository.save(chatRoom2)
+                val chatRoom1 = chatRoomRepository.save(ChatRoom(name = "chatroom-1", owner = user1))
+                val chatRoom2 = chatRoomRepository.save(ChatRoom(name = "chatroom-2", owner = user2))
+
+                val chatRoom1Participant1 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom1, user1))
+                val chatRoom1Participant2 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom1, user2))
+
+                val chatRoom2Participant1 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom2, user1))
+                val chatRoom2Participant2 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom2, user2))
             } catch (e: Exception) {
                 logger.info { "Already exists" }
             } finally {
--- Websocket/chat-sample/src/main/kotlin/xyz/mon0mon/chatsample/service/chat/ScheduledChatMessagingService.kt
+++ Websocket/chat-sample/src/main/kotlin/xyz/mon0mon/chatsample/service/chat/ScheduledChatMessagingService.kt
@@ -15,23 +15,22 @@ class ScheduledChatMessagingService(
     private val chatRoomRepository: ChatRoomRepository
 ) {
 
-    @Scheduled(fixedRate = 10_000)
+    // 10초마다 모든 채널에 메시지를 보냅니다.
+//    @Scheduled(fixedRate = 10_000)
+    @Scheduled(fixedRate = 2_000)
     fun sendScheduledMessage() {
 
         logger.debug { "Broadcasting Scheduled Message to All Channels" }
 
         val chatRooms = chatRoomRepository.findAll()
 
-        chatRooms.forEach {
-            val scheduledChatMessage = ScheduledChatMessage(roomId = it.id!!)
-            messageSendingOperations.convertAndSend("/topic/periodic", scheduledChatMessage)
-            messageSendingOperations.convertAndSend("/topic/chat/${it.id}", scheduledChatMessage)
-        }
+        val scheduledChatMessage = ScheduledChatMessage()
+        messageSendingOperations.convertAndSend("/topic/periodic", scheduledChatMessage)
     }
 }
 
 data class ScheduledChatMessage(
-    val roomId: Long,
+    val roomId: Long? = null,
     val message: String = "server periodic message",
     val timestamp: OffsetDateTime = OffsetDateTime.now()
 )
