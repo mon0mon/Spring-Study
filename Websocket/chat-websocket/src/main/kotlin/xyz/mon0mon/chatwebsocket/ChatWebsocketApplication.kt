3b6e2766460f9c696a4f64b8cef7cb4176b54b48
mon0mon <mon0mon@naver.com>
2025-03-22 13:54:33 +0900

    #7 Spring Websocket - STOMP Communication feat: Chat Websocket 프로젝트 - Application 초기 실행 데이터 추가

--- Websocket/chat-websocket/src/main/kotlin/xyz/mon0mon/chatwebsocket/ChatWebsocketApplication.kt
+++ Websocket/chat-websocket/src/main/kotlin/xyz/mon0mon/chatwebsocket/ChatWebsocketApplication.kt
@@ -1,10 +1,77 @@
 package xyz.mon0mon.chatwebsocket
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.security.crypto.password.PasswordEncoder
+import xyz.mon0mon.chatwebsocket.domain.chat.ChatMessage
+import xyz.mon0mon.chatwebsocket.domain.chat.ChatRoom
+import xyz.mon0mon.chatwebsocket.domain.chat.ChatRoomParticipant
+import xyz.mon0mon.chatwebsocket.domain.user.User
+import xyz.mon0mon.chatwebsocket.repository.chat.ChatMessageRepository
+import xyz.mon0mon.chatwebsocket.repository.chat.ChatRoomParticipantRepository
+import xyz.mon0mon.chatwebsocket.repository.chat.ChatRoomRepository
+import xyz.mon0mon.chatwebsocket.repository.user.UserRepository
+
+private val logger = KotlinLogging.logger { }
 
 @SpringBootApplication
-class ChatWebsocketApplication
+@EnableScheduling
+class ChatWebsocketApplication {
+    @Bean
+    fun init(
+        userRepository: UserRepository,
+        chatRoomRepository: ChatRoomRepository,
+        chatMessageRepository: ChatMessageRepository,
+        chatRoomParticipantRepository: ChatRoomParticipantRepository,
+        passwordEncoder: PasswordEncoder
+    ) = CommandLineRunner {
+        try {
+            val user1 =
+                userRepository.save(
+                    User(name = "user1", email = "user1@example.com", password = passwordEncoder.encode("1234"))
+                )
+            val user2 =
+                userRepository.save(
+                    User(name = "user2", email = "user2@example.com", password = passwordEncoder.encode("1234"))
+                )
+
+            val chatRoom1 = chatRoomRepository.save(ChatRoom(name = "chatroom-1", owner = user1))
+            val chatRoom2 = chatRoomRepository.save(ChatRoom(name = "chatroom-2", owner = user2))
+
+            val chatRoom1Participant1 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom1, user1))
+            val chatRoom1Participant2 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom1, user2))
+
+            val chatRoom2Participant1 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom2, user1))
+            val chatRoom2Participant2 = chatRoomParticipantRepository.save(ChatRoomParticipant(chatRoom2, user2))
+
+            val chatMessage1 =
+                chatMessageRepository.save(
+                    ChatMessage(sender = user1, content = "Hello, Java!", chatRoom = chatRoom1)
+                )
+            val chatMessage2 =
+                chatMessageRepository.save(
+                    ChatMessage(sender = user2, content = "Hello, Kotlin!!", chatRoom = chatRoom1)
+                )
+
+            val chatMessage3 =
+                chatMessageRepository.save(
+                    ChatMessage(sender = user1, content = "Hello, Java!", chatRoom = chatRoom2)
+                )
+            val chatMessage4 =
+                chatMessageRepository.save(
+                    ChatMessage(sender = user2, content = "Hello, Kotlin!!", chatRoom = chatRoom2)
+                )
+        } catch (e: Exception) {
+            logger.info { "Already exists" }
+        } finally {
+            logger.info { "Finish Inserting Data" }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ChatWebsocketApplication>(*args)
