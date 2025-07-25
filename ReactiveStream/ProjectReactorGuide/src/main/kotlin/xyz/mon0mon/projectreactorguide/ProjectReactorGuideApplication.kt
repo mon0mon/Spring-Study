beff01109a3858713c39a1bfe81bd41921c828fe
Max Lee <max.lee@neoventures.kr>
2025-07-25 17:48:15 +0900

    #9 Reactive Stream - Project Reactor init: Project Reactor 프로젝트 - 신규 프로젝트 시작

---
+++ ReactiveStream/ProjectReactorGuide/src/main/kotlin/xyz/mon0mon/projectreactorguide/ProjectReactorGuideApplication.kt
@@ -0,0 +1,11 @@
+package xyz.mon0mon.projectreactorguide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectReactorGuideApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectReactorGuideApplication>(*args)
+}
